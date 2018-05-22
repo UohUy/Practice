fb9384aa99257f9ba9d274ec4e74c3d77e40281a
Administrator <houyu2006415@163.com>
2018-05-22 19:17:29 +1000

    Finish comment.

--- FIT5047 - Assignment/FIT5047_28585631_Hou_1stSem2018_Asst2.py
+++ FIT5047 - Assignment/FIT5047_28585631_Hou_1stSem2018_Asst2.py
@@ -4,9 +4,15 @@ import math
 import random
 import time
 
+"""
+Some shorten expressions of methods which makes easier to call
+"""
 sin = math.sin
 pi = math.pi
 exp = math.exp
+"""
+Some basic values that used in annealing algorithm
+"""
 T_START = 1000
 T_MIN = pow(10, -7)
 delta = 0.0001
@@ -21,35 +27,82 @@ class Task2:
 
     @staticmethod
     def random_y():
+        """
+        Randomly select a value in three possible choice [1, 0, -1]
+        :return: a value selected in [1, 0, -1]
+        """
         return random.choice([1, 0, -1])
 
     @staticmethod
     def random_x():
+        """
+        Random generated a value in [0, 1]
+        :return: a random value in [0, 1]
+        """
         return random.randint(0, 10000001) / 10000000.0
 
     @staticmethod
     def random_variable():
+        """
+        Variables is generated randomly in [-1, 1]
+        Used for generating value for u, v, w
+        :return: a random value in [-1, 1]
+        """
         return random.uniform(0, 10000001) / 5000000.0 - 1
 
     @staticmethod
     def func_2a(u):
+        """
+        Formulation in 2a
+        Required one variable u and return its result after calculation
+        :param u: variable that required by the formulation
+        :return: result of the formulation in 2a
+        """
         return u * sin(1 / (0.01 + u ** 2)) + u ** 3 * sin(1 / (0.001 + u ** 4))
 
     @staticmethod
     def func_2b(u, v):
+        """
+        Formulation in 2b
+        Required two variable u and v and return its result after calculation
+        :param u: the first variable that required by the formulation
+        :param v: the second variable that required by the formulation
+        :return: result of the formulation in 2b
+        """
         return u * v ** 2 * sin(v / (0.01 + u ** 2)) + u ** 3 * v ** 2 * sin(
             v ** 3 / (0.001 + u ** 4))
 
     @staticmethod
     def func_2c(u, v, w):
+        """
+        Formulation in 2c
+        Required three variable u , v, and w and return its result after calculation
+        :param u: the first variable that required by the formulation
+        :param v: the second variable that required by the formulation
+        :param w: the third variable that required by the formulation
+        :return: result of the formulation in 2c
+        """
         return (u * v ** 2 + sin(pi * w)) * sin(v / (0.01 + u ** 2)) * sin(pi * w / 2) + u ** 3 * v ** 2 * w * sin(
             v ** 3 / (0.001 * sin(pi * w / 2) ** 2 + u ** 4 + (w - 1) ** 2))
 
     @staticmethod
     def func_2d(u, v, w, y):
+        """
+        Formulation in 2d
+        Required four variable u , v, w and y and return its result after calculation
+        :param u: the first variable that required by the formulation
+        :param v: the second variable that required by the formulation
+        :param w: the third variable that required by the formulation
+        :param y: the fourth d variable that required by the formulation
+        :return: result of the formulation in 2c
+        """
         return Task2.func_2c(u, v, w, ) * y
 
     def update_p(self, new_result):
+        """
+        This method if used for calculate the newest p based on the new result and current t
+        :param new_result:
+        """
         diff = abs(new_result - self.result)
         t = self.t
 
@@ -60,6 +113,10 @@ class Task2:
             self.p = 1 / (1 + exp(diff / t))
 
     def get_2a_result(self, u=random_variable()):
+        """
+        Using annealing algorithm to calculate the highest point in formulation 2a
+        :param u: the variable that required by the formulation, default value is assigned randomly in [-1, 1]
+        """
         i = 0
         start = time.clock()
         self.result = self.func_2a(u)
@@ -81,6 +138,11 @@ class Task2:
         print('%d times simulation. Cost %f second' % (i, (end - start)))
 
     def get_2b_result(self, u=random_variable(), v=random_variable()):
+        """
+        Using annealing algorithm to calculate the highest point in formulation 2a
+        :param u: the variable that required by the formulation, default value is assigned randomly in [-1, 1]
+        :param v: the variable that required by the formulation, default value is assigned randomly in [-1, 1]
+        """
         i = 0
         start = time.clock()
         self.result = self.func_2b(u, v)
@@ -103,6 +165,12 @@ class Task2:
         print(i, "times simulation. Cost %f second" % (end - start))
 
     def get_2c_result(self, u=random_variable(), v=random_variable(), w=random_variable()):
+        """
+        Using annealing algorithm to calculate the highest point in formulation 2a
+        :param u: the variable that required by the formulation, default value is assigned randomly in [-1, 1]
+        :param v: the variable that required by the formulation, default value is assigned randomly in [-1, 1]
+        :param w: the variable that required by the formulation, default value is assigned randomly in [-1, 1]
+        """
         i = 0
         start = time.clock()
         self.result = self.func_2c(u, v, w)
@@ -126,6 +194,13 @@ class Task2:
         print('%d times simulation. Cost %f second' % (i, (end - start)))
 
     def get_2d_result(self, u=random_variable(), v=random_variable(), w=random_variable(), y=random_y()):
+        """
+        Using annealing algorithm to calculate the highest point in formulation 2a
+        :param u: the variable that required by the formulation, default value is assigned randomly in [-1, 1]
+        :param v: the variable that required by the formulation, default value is assigned randomly in [-1, 1]
+        :param w: the variable that required by the formulation, default value is assigned randomly in [-1, 1]
+        :param y: the variable that required by the formulation, default value is selected randomly in [-1, 0, 1]
+        """
         i = 0
         start = time.clock()
         self.result = self.func_2d(u, v, w, y)
@@ -151,6 +226,10 @@ class Task2:
 
 
 def display_2a_menu():
+    """
+    Print text-based UI and ask user for select which value that the annealing
+    Process should start with in 2a.
+    """
     while True:
         print("---------2a---------")
         print("1. Variables start as 0")
@@ -170,6 +249,10 @@ def display_2a_menu():
 
 
 def display_2b_menu():
+    """
+    Print text-based UI and ask user for select which value that the annealing
+    Process should start with in 2b.
+    """
     while True:
         print("---------2a---------")
         print("1. Variables start as 0")
@@ -190,6 +273,10 @@ def display_2b_menu():
 
 
 def display_2c_menu():
+    """
+    Print text-based UI and ask user for select which value that the annealing
+    Process should start with in 2c.
+    """
     while True:
         print("---------2a---------")
         print("1. Variables start as 0")
@@ -210,6 +297,10 @@ def display_2c_menu():
 
 
 def display_2d_menu():
+    """
+    Print text-based UI and ask user for select which value that the annealing
+    Process should start with in 2d.
+    """
     while True:
         print("---------2a---------")
         print("1. Variables start as 0")
@@ -228,7 +319,11 @@ def display_2d_menu():
             u, v, w, y = variables.split(',')
             task2.get_2d_result(u, v, w, y)
 
+
 def run():
+    """
+    Show the main menu and select which task should run
+    """
     while True:
         print("---------Task 2---------")
         print("1. 2a")
